4b2d433d29be2962381ddac2db909821cb0e6e82
Rajibul <rajibul.hasan@yaana.com>
2025-10-21 18:11:13 +0600

    Readme Updated

--- src/components/shared/Counter.tsx
+++ src/components/shared/Counter.tsx
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increament } from '../../store/slices/counterSlice';
 import type { Rootstate } from '../../store/store';
 const Counter = () => {
     const count=useSelector((state:Rootstate)=>state.coun.value)
+    const dispatch=useDispatch();
   return (
-    <div className='text-orange-300'>Counter: {count}</div>
+    <>
+        <div className='text-orange-300'>Counter: {count}</div>
+        <button onClick={()=>dispatch(increament())}>Plus</button>
+        <button onClick={()=>dispatch(decrement())}>Minus</button>
+    </>
+    
   )
 }
 
